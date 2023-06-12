2ac134486ce6ffb67ab9f9889bdbb47f864a2b9d
chetanya joshi <chaitanyajoshi173@gmai.com>
2023-06-12 17:45:55 +0530

    final calculator

--- cal.py
+++ cal.py
@@ -1,30 +1,34 @@
 # This function adds two numbers
-import Multiplication
-import Division
+import Addition
+import Subtraction
+
 
 # This is to present a menu to the user
 print("Select operation.")
-print("3.Multiply")
-print("4.Divide")
+print("1.Addition")
+print("2.Subtract")
+
 
 while True:
     # take input from the user
-    choice = input("Enter choice(3/4): ")
+    choice = input("Enter choice(1/2): ")
 
     # check if choice is one of the four options
-    if choice in ('3', '4'):
+    if choice in ('1', '2'):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
         except ValueError:
             print("Invalid input. Please enter a number.")
             continue
-        elif choice == '3':
-            print(num1, "*", num2, "=", Multiplication.multiply(num1, num2))
 
-        elif choice == '4':
-            print(num1, "/", num2, "=", Division.divide(num1, num2))
-        
+        if choice == '1':
+            print(num1, "+", num2, "=", Addition.add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", Subtraction.subtract(num1, num2))
+
+                
         # check if user wants another calculation
         # break the while loop if answer is no
         next_calculation = input("Let's do next calculation? (yes/no): ")
